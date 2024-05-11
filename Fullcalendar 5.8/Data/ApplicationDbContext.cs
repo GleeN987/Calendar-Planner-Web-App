d9408d0205e713b0ef24c0055ee0620fd87bb3dc
GleeN987 <gleenmarcin@gmail.com>
2024-05-11 18:03:56 +0200

    dodanie pages

--- Fullcalendar 5.8/Data/ApplicationDbContext.cs
+++ Fullcalendar 5.8/Data/ApplicationDbContext.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Fullcalendar_5._8.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Fullcalendar_5._8.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
     }
 }
---
+++ Fullcalendar 5.8/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Fullcalendar_5._8.Models
+{
+    public class User : IdentityUser
+    {
+
+    }
+}
